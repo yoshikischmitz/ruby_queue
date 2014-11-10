d7517d8a633f47d432d896a19e844abb2aeb8166
Yoshiki <yoshikischmitz@outlook.com>
2014-11-10 00:55:15 -0700

    reimplement circular buffer

--- circular_buffer.rb
+++ circular_buffer.rb
@@ -1,26 +1,35 @@
 class CircularBuffer
-  BUFFER_SIZE = 100
+  BUFFER_SIZE = 10
 
   def initialize
-    @buffer_array = []
+    @buffer_array = Array.new(BUFFER_SIZE)
+    @count = 0
     @out_counter = 0
+    @in_counter = 0
   end
 
   def <<(val)
-    @buffer_array << val
+    @buffer_array[@in_counter % BUFFER_SIZE] = val
+    @count += 1
+    @in_counter += 1
     @buffer_array
   end
 
   def pop
-    @buffer_array.shift
+    idx = @out_counter % BUFFER_SIZE
+    val = @buffer_array[idx]
+    @buffer_array[idx] = nil
+    @count -= 1
+    @out_counter += 1
+    val
   end
 
   def any?
-    @buffer_array.size > 0
+    @count > 0
   end
 
   def open?
-    @buffer_array.size < BUFFER_SIZE
+    @count < BUFFER_SIZE
   end
 
   def to_s
